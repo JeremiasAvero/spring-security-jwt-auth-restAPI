00142d87f467bf7db8cc72112124ccfbc2ded4ff
ProyectosTotoras <proyectostotoras@gmail.com>
2025-10-25 10:00:05 -0300

    first commit

---
+++ src/main/java/com/jeremiasAvero/app/SpringSecurityJwtAuthRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.jeremiasAvero.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAuthRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtAuthRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeremiasAvero/app/SpringSecurityJwtAuthRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremiasAvero.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtAuthRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
